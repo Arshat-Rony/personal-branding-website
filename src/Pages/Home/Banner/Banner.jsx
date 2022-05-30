93fb5e2f655d67138fc1902a5a66ecfa7a870e34
Rony <frrony1996@gmail.com>
2022-05-30 19:25:26 +0500

    heading changes

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -8,8 +8,8 @@ const Banner = () => {
         <div className='banner d-flex justify-content-center ailign-items-start flex-column'>
             <div className="banner-text text-start container">
                 <h1 className='heading text-primary'>Emily Jhonson</h1>
-                <p>Your favourite doctor is Here</p>
-                <h3 >Modern Dentistry in calm and relaxed here</h3>
+                <p>Your favourite doctor Is Here</p>
+                <h3 >Modern Dentistry in calm and relaxed Here</h3>
                 <Link to="/checkout">
                     <Button text={"Make Appointment"}></Button>
                 </Link>
